62ea01824892513ed5100c215cf3cd3fc3b237a9
Roger Sobrado <roger.sobrado.e@gmail.com>
2020-12-28 09:22:53 -0600

    Update palindrome.js

--- src/javascript-test/palindrome.js
+++ src/javascript-test/palindrome.js
@@ -1,1 +1,13 @@
+
 /* Export your NodeJS module from this file */
+const isPalindrome = (str) =>{
+  
+  if  (typeof(str) !== 'string')  throw {error  : "input must be wrapped into ' ' "};  
+  
+  var re = /[\W_]/g; //  var re = /[^A-Za-z0-9]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join('');
+
+  if (str==="")  return false
+  return reverseStr === lowRegStr; 
+}
